55d0f62753da1ed2b2d47bd4e6875a4e2ee37b65
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-09-12 18:54:50 +0100

    setup allauth

--- salon_management/settings.py
+++ salon_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -59,13 +63,35 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', #required by allauth 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email' # Use either username or email 
+ACCOUNT_EMAIL_REQUIRED = True #require email
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory' # Must verify email through link
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True #enter twice for no typos 
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/' #redirect after login to homepage
 
 WSGI_APPLICATION = 'salon_management.wsgi.application'
 
