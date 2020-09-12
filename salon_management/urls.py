b3e02a61bc4c45bfda35a7e49b53b0fa91f65e95
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-09-12 19:25:28 +0100

    added home app and templates

--- salon_management/urls.py
+++ salon_management/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')) #all urls needed for login / logout etc 
+    path('accounts/', include('allauth.urls')), #all urls needed for login / logout etc 
+    path('', include('home.urls')) 
 ]
