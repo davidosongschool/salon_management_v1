c27495bc2b604e818e235f549f69a05e5388d0a9
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-09-15 21:22:15 +0100

    added product views and templates

---
+++ products/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
